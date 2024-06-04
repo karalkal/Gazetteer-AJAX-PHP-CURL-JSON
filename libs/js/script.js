663ec75fe84b6cdc39d4b1c7aee79e47dc8e69e1
karalkal <karalkal@yahoo.co.uk>
2024-06-04 15:45:44 +0100

    first modal completed

--- libs/js/script.js
+++ libs/js/script.js
@@ -236,6 +236,11 @@ $(document).ready(function () {
 		$("#countryName2").text(`(${data.altSpellings[data.altSpellings.length - 1]})`);
 		$("#countryFlag").html(`<img src="${data.flag}"/>`);
 		$("#countryCoatOfArms").html(`<img src="${data.coatOfArms}"/>`);
+		$("#countryArea").html(`<span>${Intl.NumberFormat('de-DE').format(data.area)} km&#178;</span>`);
+		$("#countryPopulation").html(`<span>${Intl.NumberFormat('de-DE').format(data.population)}`);
+		$("#countryCapital").html(`<span>${data.capital}</span>`);
+		$("#countryTLD").html(`<span>${data.tld}</span>`);
+
 	}
 
 
